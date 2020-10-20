31b99ce784a88ef086c51ccecccfc8543a73152a
Bob Carey <rpc5102@psu.edu>
2020-10-20 13:41:23 -0400

    Updated: Examples

--- inst/examples/app.R
+++ inst/examples/app.R
@@ -1,30 +1,135 @@
+library(boastUtils)
 library(boastWidgets)
 
+APP_TITLE <<- "boastWidgets"
+
 # Define UI for app that draws a histogram ----
-ui <- fluidPage(
-
-  # App title ----
-  titlePanel("Hello Shiny!"),
-
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
+ui <- list(
+  ## Create the app page
+  dashboardPage(
+    skin = "blue",
+    ### Create the app header
+    dashboardHeader(
+      title = "boastWidgets", # You may use a shortened form of the title here
+      tags$li(class = "dropdown", actionLink("info", icon("info"))),
+      tags$li(class = "dropdown",
+              tags$a(href='https://github.com/EducationShinyAppTeam/BOAST',
+                     icon("github"))),
+      tags$li(class = "dropdown",
+              tags$a(href='https://shinyapps.science.psu.edu/',
+                     icon("home")))
     ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Poll
-      radialPollOutput("poll")
+    ### Create the sidebar/left navigation menu
+    dashboardSidebar(
+      sidebarMenu(
+        id = "pages",
+        menuItem("Basic", tabName = "Basic", icon = icon("thermometer-quarter")),
+        menuItem("Advanced", tabName = "Advanced", icon = icon("thermometer-three-quarters"))
+      ),
+      tags$div(
+        class = "sidebar-logo",
+        boastUtils::psu_eberly_logo("reversed")
+      )
+    ),
+    ### Create the content
+    dashboardBody(
+      tabItems(
+        #### Set up the Overview Page
+        tabItem(
+          tabName = "Basic",
+          fluidRow(
+            # Basic Examples
+            # https://shiny.rstudio.com/tutorial/written-tutorial/lesson3/ 
+            column(3,
+                   h3("Buttons"),
+                   actionButton("action", "Action"),
+                   br(),
+                   br(), 
+                   submitButton("Submit")),
+            
+            column(3,
+                   h3("Single checkbox"),
+                   checkboxInput("checkbox", "Choice A", value = TRUE)),
+            
+            column(3, 
+                   checkboxGroupInput("checkGroup", 
+                                      h3("Checkbox group"), 
+                                      choices = list("Choice 1" = 1, 
+                                                     "Choice 2" = 2, 
+                                                     "Choice 3" = 3),
+                                      selected = 1)),
+            
+            column(3, 
+                   dateInput("date", 
+                             h3("Date input"), 
+                             value = "2014-01-01"))   
+          ),
+          
+          fluidRow(
+            
+            column(3,
+                   dateRangeInput("dates", h3("Date range"))),
+            
+            column(3,
+                   fileInput("file", h3("File input"))),
+            
+            column(3, 
+                   h3("Help text"),
+                   helpText("Note: help text isn't a true widget,", 
+                            "but it provides an easy way to add text to",
+                            "accompany other widgets.")),
+            
+            column(3, 
+                   numericInput("num", 
+                                h3("Numeric input"), 
+                                value = 1))   
+          ),
+          
+          fluidRow(
+            
+            column(3,
+                   radioButtons("radio", h3("Radio buttons"),
+                                choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                               "Choice 3" = 3),selected = 1)),
+            
+            column(3,
+                   selectInput("select", h3("Select box"), 
+                               choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                              "Choice 3" = 3), selected = 1)),
+            
+            column(3, 
+                   sliderInput("slider1", h3("Sliders"),
+                               min = 0, max = 100, value = 50),
+                   sliderInput("slider2", "",
+                               min = 0, max = 100, value = c(25, 75))
+            ),
+            
+            column(3, 
+                   textInput("text", h3("Text input"), 
+                             value = "Enter text..."))   
+          )
+        ),
+        #### Note: you must have at least one of the following pages. You might
+        #### have more than one type and/or more than one of the same type. This
+        #### will be up to you and the goals for your app.
+        #### Set up an Explore Page
+        tabItem(
+          tabName = "Advanced",
+          # Output: Poll
+          fluidRow(
+            column(9,
+              h2("Radial Poll"),
+              radialPollOutput("poll")
+            )
+          )
+        )
+      )
     )
   )
 )
 
 # Define server logic required to draw a histogram ----
-server <- function(input, output) {
+server <- function(input, output, session) {
   data <- jsonlite::read_json('./sample-data.json')
 
   output$poll <- renderRadialPoll({
@@ -51,4 +156,5 @@ server <- function(input, output) {
   })
 }
 
-shinyApp(ui, server)
+# Boast App Call ----
+boastUtils::boastApp(ui = ui, server = server)
